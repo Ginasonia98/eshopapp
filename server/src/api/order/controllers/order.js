fbe183660b5619bf36927aa5fea12e0dbae3d99c
Ginachiko1998 <soniagina669@gmail.com>
2023-03-11 00:02:10 +0700

    make change

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -8,62 +8,62 @@ order controller
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
-async create(ctx) {
-const { products, userName, email } = ctx.request.body;
-try {
-  const lineItems = await Promise.all(
-    products.map(async (product) => {
-      const item = await strapi.service("api::item.item").findOne(product.id);
-      return {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: item.name,
-          },
-          unit_amount: item.price * 100,
-        },
-        quantity: product.count,
-      };
-    })
-  );
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+    console.log({ products });
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
 
-  // log lineItems to check the values
-  console.log("lineItems", lineItems);
+      // log lineItems to check the values
+      console.log("lineItems", lineItems);
+      // create a stripe session
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
 
-  // create a stripe session
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    customer_email: email,
-    mode: "payment",
-    success_url: "http://localhost:3000/checkout/success",
-    cancel_url: "http://localhost:3000",
-    line_items: lineItems,
-  });
+      // log session to check the values
+      console.log("session", session);
 
-  // log session to check the values
-  console.log("session", session);
+      // create the item
 
-  // create the item
-  await strapi.service("api::order.order").create({ 
-    userName, 
-    products, 
-    stripeSessionId: session.id ,
-    
-  });
+      try {
+        const data = {
+          userName,
+          products,
+          stripeSessionId: session.id,
+        };
+        await strapi.entityService.create("api::order.order", { data });
+      } catch (error) {
+        console.log("error nih", { error });
+      }
 
-  // redirect to checkout
-  return { id: session.id };
-} catch (error) {
-  console.error("Error:", error);
-  ctx.response.status = 500;
-  return { error: { message: "There was a problem creating the charge" } };
-}
-},
+      // // redirect to checkout
+      return { id: session.id };
+    } catch (error) {
+      console.error("Error:", error);
+      ctx.response.status = 500;
+      return { error: { message: "There was a problem creating the charge" } };
+    }
+  },
 }));
-
-
-
-
-
-
-
